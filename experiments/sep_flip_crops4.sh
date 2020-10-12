c1647cb30e5e2f0026fabfbd3bac32b2f528ebee
juancprzs <jc.perez13@uniandes.edu.co>
2020-10-12 08:40:34 -0500

    new experiments with separate flip and crop

---
+++ experiments/sep_flip_crops4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --partition=batch
+#SBATCH --array=[0-2]
+#SBATCH -J sep_flip_crops4
+#SBATCH -o logs/sep_flip_crops4.%J.out
+#SBATCH -e logs/sep_flip_crops4.%J.err
+#SBATCH --time=6:00:00
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=2
+#SBATCH --mail-user=perezjc@kaust.edu.sa
+#SBATCH --mail-type=ALL
+
+python main.py --test-samples 2500 --seed ${SLURM_ARRAY_TASK_ID} \
+--checkpoint runs/sample_sep_flip_crops4_run${SLURM_ARRAY_TASK_ID} \
+--n-crops 4 \
+--flip
