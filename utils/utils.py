526f9bc87e7f745dc5ba8407551be5c18bb599b2
juancprzs <jc.perez13@uniandes.edu.co>
2020-10-15 08:27:29 -0500

    minor error correction

--- utils/utils.py
+++ utils/utils.py
@@ -164,7 +164,7 @@ def get_data_utils(batch_size, test_samples=None, chunks=None, num_chunk=None):
         import pdb; pdb.set_trace()
         data = [testset[i] for i in range(start_ind, end_ind)]
         imgs = torch.cat([x.unsqueeze(0) for (x, y) in data], 0)
-        labs = torch.cat([y.unsqueeze(0) for (x, y) in data], 0)
+        labs = torch.cat([torch.tensor(y).unsqueeze(0) for (x, y) in data], 0)
 
     testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, 
                             num_workers=2, pin_memory=True, drop_last=False)
