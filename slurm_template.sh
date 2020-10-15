bd2a5bf00f7c837f6ee825b9fa98303bc51fe6b3
juancprzs <jc.perez13@uniandes.edu.co>
2020-10-15 17:50:22 -0500

    template for wasy setup

---
+++ slurm_template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --partition=batch
+#SBATCH --array=[1-20]
+#SBATCH -J chunked_baseline
+#SBATCH -o logs/chunked_baseline.%J.out
+#SBATCH -e logs/chunked_baseline.%J.err
+#SBATCH --time=4:00:00
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=2
+#SBATCH --mail-user=perezjc@kaust.edu.sa
+#SBATCH --mail-type=ALL
+#SBATCH -A conf-gpu-2020.11.23
+
+python main.py \
+--checkpoint runs/chunked_baseline \
+--num-chunk ${SLURM_ARRAY_TASK_ID} \
+--chunks 20
