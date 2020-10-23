daa0a833fde55c15657f47d2ad982847a6cb4f34
Motasem Alfarra <alfarrm@kw60249.cbrc.kaust.edu.sa>
2020-10-23 21:18:00 +0300

    adding unlabeled to utils

--- utils/utils.py
+++ utils/utils.py
@@ -277,6 +277,9 @@ def get_model(experiment):
     elif experiment == 'imagenet_pretraining': # ImageNet preatraining
         from experiments.imagenet_pretraining import get_imagenet_pretrained_model
         model = get_imagenet_pretrained_model()
+    elif experiment == 'unlabeled_pretraining': # Unlabeled preatraining
+        from experiments.unlabeled_pretraining import get_imagenet_pretrained_model
+        model = get_imagenet_pretrained_model()
 
     return model
 
