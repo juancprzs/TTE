b56c179511620947d8abb23bf21b1f3c0453a922
guillaumejs2403 <gjeanneret10@uniandes.edu.co>
2020-11-03 17:41:34 -0500

    finally it works!

--- imagenet-fd-numpy.py
+++ imagenet-fd-numpy.py
@@ -44,7 +44,7 @@ def arguments():
     parser.add_argument('--flip', action='store_true')
     parser.add_argument('--n-crops', default=0, type=int)
     parser.add_argument('--flip-crop', action='store_true')
-    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--seed', type=int, default=1)
 
     # Attack options
     parser.add_argument('--epsilon', type=float, default=8 / 255,
@@ -61,6 +61,39 @@ def arguments():
 
 # ========================================
 # Chunks dataset
+
+
+class DataLoader():
+    def __init__(self, dataset, batch_size):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.n = 0
+
+        self.n_iters = len(dataset) / batch_size
+        self.n_iters = int(self.n_iters) + 1 if (self.n_iters % 1) != 0 else int(self.n_iters)
+
+    def __len__(self):
+        return self.n_iters
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+
+        if self.n >= self.n_iters:
+            raise StopIteration
+
+        current = self.n * self.batch_size
+        next = current + self.batch_size
+
+        data = [self.dataset[idx] for idx in range(current, min(next, len(self.dataset)))]
+        images = np.concatenate([data[idx][0] for idx in range(len(data))], axis=0)
+        labels = np.concatenate([data[idx][1] for idx in range(len(data))], axis=0)
+
+        self.n += 1
+
+        return images, labels
 
 
 class ChunkDataset():
@@ -93,10 +126,51 @@ def compute_corrects(features, label):
     return np.sum(correct), pred
 
 
-def loader(path):
-    im = cv2.imread(path, cv2.IMREAD_COLOR)
-    im = Image.fromarray(im)
-    return im
+def load_and_process(path, inter=cv2.INTER_CUBIC):
+    # import pdb; pdb.set_trace()
+    shortest = 256
+
+    image = cv2.imread(path)
+    
+    # resize the image
+    dim = None
+    (h, w) = image.shape[:2]
+
+    H = False
+
+    if h > w:
+        r = shortest / w
+        dim = (shortest, int(h * r))
+    elif w > h:
+        H = True
+        r = shortest / h
+        dim = (int(w * r), shortest)
+        # dim = (shortest, int(w * r))
+    else:
+        resized =  cv2.resize(image, (shortest, shortest), interpolation=inter)
+        return np.expand_dims(resized, axis=0)
+
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # center crop
+    (h, w) = resized.shape[:2]
+
+    if H:
+        center_crop = resized[:, (int(w / 2) - 128): (int(w / 2) + 128), :]
+    else:
+        center_crop = resized[(int(h / 2) - 128): (int(h / 2) + 128), :, :]
+
+    center_crop = np.expand_dims(center_crop, axis=0)
+    
+    return center_crop
+
+
+class ImageNetDataset(datasets.ImageFolder):
+    def __getitem__(self, idx):
+        path, target = self.samples[idx]
+        target = np.array([target])
+        image = load_and_process(path)
+        return image, target
 
 
 def main(args):
@@ -112,39 +186,46 @@ def main(args):
 
     # forward here
     img_input = tf.placeholder(dtype=tf.float32, shape=[None, 256, 256, 3])
-    image_ = tf.transpose(img_input, [0, 3, 1, 2])
+    image_ = img_input * 2.0 - 1.0
+    # image_ = img_input / 127.5 - 1.0
+    image_ = tf.transpose(image_, [0, 3, 1, 2])
     lab_input = tf.placeholder(dtype=tf.int64, shape=[None])
     with TowerContext('', is_training=False):
         logits = model.get_logits(image_)
 
     # Session and load weights
-    sess = tf.Session()
+    sess = tf.compat.v1.Session()
     get_model_loader(args.load).init(sess)
 
     model_adapted = utils_tf.ModelAdapter(logits, img_input, lab_input, sess, num_classes=1000)
 
     # load dataset
-    normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5],
-                                     std=[0.5, 0.5, 0.5])  # the model normalizes everything inside
-    val_transform = transforms.Compose([
-            transforms.Resize(256),  # Shortest edge will become 256
-            transforms.CenterCrop(256),  # CenterCrop of 256, the model internally takes the 224 x 224 crop
-            transforms.ToTensor(),
-            normalize,
-            TORCH_TO_TF()  # The model takes as input a BGR image
-        ])
-    dataset = datasets.ImageFolder(os.path.join(args.data, 'val'), transform=val_transform)
+    # normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5],
+    #                                  std=[0.5, 0.5, 0.5])  # the model normalizes everything inside
+    # val_transform = transforms.Compose([
+    #         transforms.Resize(256),  # Shortest edge will become 256
+    #         transforms.CenterCrop(256),  # CenterCrop of 256, the model internally takes the 224 x 224 crop
+    #         transforms.ToTensor(),
+    #         normalize,
+    #         TORCH_TO_TF()  # The model takes as input a BGR image
+    #     ])
+    # dataset = datasets.ImageFolder(os.path.join(args.data, 'val'), transform=val_transform)
+    # chunkloader = ChunkDataset(dataset, num_chunk=args.actual_chunk,
+    #                            total_chunks=args.total_chunks)
+    # dataloader = torch.utils.data.DataLoader(chunkloader, batch_size=args.batch,
+    #                                          shuffle=False, num_workers=8,
+    #                                          pin_memory=True)
+
+    dataset = ImageNetDataset(os.path.join(args.data, 'val'))
     chunkloader = ChunkDataset(dataset, num_chunk=args.actual_chunk,
                                total_chunks=args.total_chunks)
-    dataloader = torch.utils.data.DataLoader(chunkloader, batch_size=args.batch,
-                                             shuffle=False, num_workers=8,
-                                             pin_memory=True)
+    dataloader = DataLoader(chunkloader, args.batch)
 
     print('\n\n\nTOTAL IMAGES ON THIS CHUNK: {}'.format(len(chunkloader)))
 
     # Create adversary
-    adversary = AutoAttack(model_adapted, norm='Linf', eps=args.epsilon,
-                           version='standard', is_tf_model=True, verbose=False)
+    adversary = AutoAttack(model_adapted, norm='Linf', eps=8.0 / 255.0,
+                           version='standard', is_tf_model=True, verbose=True)
 
     # Count variables
     n = 0
@@ -157,29 +238,31 @@ def main(args):
         advers_pred = []
         adv_examples = []
 
-    for clean_x, label in tqdm.tqdm(dataloader):
+    for numpy_clean, label in tqdm.tqdm(dataloader):
 
-        numpy_clean = np.transpose(clean_x.numpy(), [0, 2, 3, 1])
+        numpy_clean = numpy_clean.astype('float32') / 255.0
 
         # compute clean acc
         features = sess.run(logits, feed_dict={img_input: numpy_clean})
-        clean_correct, clean_pred = compute_corrects(features, label.numpy())
+        clean_correct, clean_pred = compute_corrects(features, label)
+
+        torch_clean = torch.from_numpy(np.transpose(numpy_clean, [0, 3, 1, 2])).to(dtype=torch.float)
 
         # compute adversary images
-        adv_x = adversary.run_standard_evaluation(clean_x, label, bs=args.batch)
+        adv_x = adversary.run_standard_evaluation(torch_clean.contiguous(), torch.from_numpy(label), bs=args.batch)
 
         numpy_adv = np.transpose(adv_x.detach().cpu().numpy(), [0, 2, 3, 1])
 
         # compute adversary acc
         adv_features = sess.run(logits, feed_dict={img_input: numpy_adv})
-        adv_correct, adv_pred = compute_corrects(adv_features, label.numpy())
+        adv_correct, adv_pred = compute_corrects(adv_features, label)
 
         c_clean += clean_correct
         c_adv += adv_correct
-        n += label.size(0)
+        n += label.shape[0]
 
         if args.save_adv:
-            labels.append(label)
+            labels.append(torch.tensor(label))
             pred.append(torch.tensor(clean_pred))
             advers_pred.append(torch.tensor(adv_pred))
             adv_examples.append(torch.tensor(numpy_adv))
